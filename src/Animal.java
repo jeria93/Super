49f80895e7c1c45ec10a234f8827f328391d52e5
nicholassamuelssonjeria <nicholasjobb.jeria@gmail.com>
2024-10-17 09:40:57 +0200

    added animal class

---
+++ src/Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+    int age;
+    String name;
+
+    public Animal() {
+
+    }
+
+    public void makeNoise() {
+        System.out.println("Hello, i am an animal");
+    }
+
+    public void eat() {
+        System.out.println("Munch Munch");
+    }
+
+}
